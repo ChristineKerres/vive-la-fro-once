7a17cb54582a4d11c09398aa828aa7612a466f3b
Tine Kerres <90827806+ChristineKerres@users.noreply.github.com>
2025-02-19 13:03:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: "/vive-la-fro-once/", // Der Name des Repositories auf GitHub
+};
